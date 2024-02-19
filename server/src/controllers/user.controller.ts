0df7ac74e9e8b6a735480a5d5b4defa16abd8650
Dhanasai Tholeti <dhanasai5566@gmail.com>
2024-02-19 19:17:34 +0530

    created a separate route for handle questions and answers

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,6 +1,28 @@
 import { Request, Response } from "express";
+import fs from "fs/promises";
+import fileModel from "models/file.model";
+import { v4 as uuidv4 } from "uuid";
 
+export const uploadFile = async (req: Request, res: Response) => {
+  {
+    try {
+      // Create a new instance of your Mongoose model
+      const newFile = await fileModel.create({
+        name: req.file.originalname,
+        file: await fs.readFile(req.file.path), // Read the file from disk
+        fileSize: req.file.size,
+      });
+      // Remove the temporary file
+      fs.unlink(req.file.path);
+      res.status(200).send("File uploaded successfully!");
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Error uploading file.");
+    }
+  }
+};
 
-
-
-export const uploadFile = (req: Request, res: Response) => {};
+export const answerTheQUestion = (req: Request, res: Response) => {
+  const randomString = uuidv4();
+  res.status(200).json({ answer: randomString });
+};
--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,9 +1,8 @@
-import { Router } from "express";
-import fileModel from "../models/file.model";
 import multer from "multer";
-import fs from "fs/promises";
-const userRoutes = Router();
+import { Router } from "express";
+import { uploadFile } from "../controllers/user.controller";
 
+const userRoutes = Router();
 // Set up Multer storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -16,21 +15,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-userRoutes.post("/", upload.single("file"), async (req, res) => {
-  try {
-    // Create a new instance of your Mongoose model
-    const newFile = await fileModel.create({
-      name: req.file.originalname,
-      file: await fs.readFile(req.file.path), // Read the file from disk
-      fileSize: req.file.size,
-    });
-    // Remove the temporary file
-    fs.unlink(req.file.path);
-    res.status(200).send("File uploaded successfully!");
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error uploading file.");
-  }
-});
+userRoutes.post("/", upload.single("file"), uploadFile);
+
+userRoutes.post("/question",);
 
 export default userRoutes;
